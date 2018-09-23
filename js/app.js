c9e63474355f1f40c0790abfbac6693874dbd083
Peter Javorkai <javorkai.peter@gmail.com>
2018-09-23 21:54:39 +0200

    Adding slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("It's loooooaded") 
+console.log("It's loooooaded")
+
+const slideHolder = document.querySelector(".slider__holder");
+const numberOfSlides = document.querySelectorAll(".slider_element").length
+
+let currentSlide = 0;
+
+setInterval(function() {
+  currentSlide += 1;
+
+  if (currentSlide === numberOfSlides) {
+    currentSlide = 0;
+  }
+
+  slideHolder.style.left = `${-100 * currentSlide}%`;
+}, 7000)
